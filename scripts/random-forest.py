a07c24e5d9cf82484ba81e6cc58588711d960327
Shiva Shamloo <shivashamloo@Shivas-MacBook-Air.local>
2020-04-10 23:16:47 -0400

    finishing random-forest

--- scripts/random-forest.py
+++ scripts/random-forest.py
@@ -4,6 +4,31 @@ from sklearn.metrics import classification_report, confusion_matrix, accuracy_sc
 from sklearn.model_selection import train_test_split
 
 
+def test():
+   
+    param_grid = {
+    'bootstrap': [True],
+    'max_depth': [80, 90, 100, 110],
+    'max_features': [2, 3],
+    'min_samples_leaf': [3, 4, 5],
+    'min_samples_split': [8, 10, 12],
+    'n_estimators': [100, 200, 300, 1000]}
+    rf = RandomForestClassifier()
+    grid_search = GridSearchCV(estimator = rf, param_grid = param_grid, 
+                          cv = 3, n_jobs = -1, verbose = 2)
+    
+    dataset = pd.read_csv('../data/crimes_dataset_processed.csv')
+    x = dataset.iloc[:, [i for i in range(8) if i != 6]].values
+    y = dataset.iloc[:, 6].values
+
+    # Get training and test sets
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+    # Fit the random search model
+
+    grid_search.fit(x_train, y_train)
+    print(grid_search.best_params_)
+
+
 def main():
     dataset = pd.read_csv('../data/crimes_dataset_processed.csv')
     x = dataset.iloc[:, [i for i in range(8) if i != 6]].values
@@ -25,4 +50,5 @@ def main():
 
 
 if __name__ == '__main__':
+    #test()
     main()
